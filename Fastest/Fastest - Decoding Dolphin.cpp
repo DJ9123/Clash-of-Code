7d5786c07ab2f72164ed7c85a1fca6cea8cc9be3
DJ Cook <DJ.Cook@live.com>
2016-12-06 02:04:44 -0600

    Added old programs

---
+++ Fastest/Fastest - Decoding Dolphin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char alphabet[27] = { ' ', 'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a' };
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int E;
+		cin >> E;
+		E++;
+		cout << alphabet[E];
+	}
+}
---
+++ Fastest/Fastest - Find Slope.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+	int b;
+	cin >> a >> b;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+
+		cout << a * x + b << endl;
+	}
+
+}
